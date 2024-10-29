7d203597d1cd98ed3d6f107d5fe53381830cde96
lou1s vuong <lou1svuong.dev@gmail.com>
2024-10-29 18:57:01 +0700

    Refactor layout component: Add wallet provider and toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { ChatSidebar } from "@/components/chat/chat-sidebar";
 import { ThemeProvider } from "next-themes";
+import { WalletProvider } from "@/components/providers/near-wallet-provider";
+import "@near-wallet-selector/modal-ui/styles.css";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,9 +34,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <SidebarProvider>
-            <main className="flex-1">{children}</main>
-          </SidebarProvider>
+          <WalletProvider>
+            <SidebarProvider>
+              <Toaster />
+              <main className="flex-1">{children}</main>
+            </SidebarProvider>
+          </WalletProvider>
         </ThemeProvider>
       </body>
     </html>
